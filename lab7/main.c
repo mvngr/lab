a1afe8f79b8ef584d954fbd006ed4dd8a24867cc
Mike <mihailqqqq@gmail.com>
2016-11-16 09:23:12 +0300

    v2

---
+++ lab7/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "matrix.h"
+
+int main()
+{
+    printMatrix(eyeMatrix(4));
+    return 0;
+}
--- lab7/matrix.c
+++ lab7/matrix.c
@@ -1,16 +1,28 @@
 #include "matrix.h"
 #include <stdio.h>
 #include <stdlib.h>
+double power(int num)
+{
+    int cpyNum = num;
+    double res = 1;
+    while (cpyNum > 0)
+    {
+        res *= 0.1;
+        cpyNum /= 10;
+    }
+    return num*res;
+}
 double ** readMatrix(char *link)
 {
     FILE *fileIn;
     double **arr;
+    int col, row;
     fileIn = fopen(link, "r");
     if (!fileIn)
         printf("AHTUNG!");
         else
         {
-            int i=0;
+            int i=0, j;
             int ch;
             ch = fgetc(fileIn);
 
@@ -112,26 +124,26 @@ double ** equateMatrix(unsigned int m, unsigned int n, double num)
             arr[i][j] = num;
     return arr;
 }
-double ** eyeMatrix(unsigned int k)
+double ** eyeMatrix(int k)
 {
     int i, j;
     double ** arr;
 
-    arr = (double **)malloc(m * sizeof(double*));
-    for (i = 0; i < m; i++)
-        arr[i]=(double *)malloc(n*sizeof(double));
+    arr = (double **)malloc(k * sizeof(double*));
+    for (i = 0; i < k; i++)
+        arr[i]=(double *)malloc(k * sizeof(double));
 
-    for (i =0; i < n; i++)
-        for (j = 0; j < m; j++)
+    for (i =0; i < k; i++)
+        for (j = 0; j < k; j++)
             if (i == j)
                 arr[i][j] = 1;
             else
                 arr[i][j] = 0;
     return arr;
 }
-double elemIndex(unsigned int col, unsigned int row, double ** arr)
+double elemIndex(unsigned int row, unsigned int col, double ** arr)
 {
-    return arr[i][j];
+    return arr[row][col];
 }
 void printMatrix(double ** matrix)
 {
@@ -146,30 +158,32 @@ void printMatrix(double ** matrix)
 }
 int saveMatrix(double ** matrix, char * link)
 {
+    int i, j;
     FILE *fileOut;
     fileOut = fopen(link, "w");
-        if (!fileOut){
+        if (!fileOut)
             return 0;
         else{
-            fprintf(fileOut, "%d, ", col);
-            fprintf(fileOut, "%d\n", row);
+            fprintf(fileOut, "%d, ", getRowsNum(matrix));
+            fprintf(fileOut, "%d\n", getColumnsNum(matrix));
             for (i = 0; i < getRowsNum(matrix); i++){
                 for (j = 0; j < getColumnsNum(matrix); j++)
-                    if(j != row-1)
-                        fprintf(fileOut,"%lf, ",arr[i][j]);
+                    if(j != getRowsNum(matrix)-1)
+                        fprintf(fileOut,"%lf, ",matrix[i][j]);
                         else
-                        fprintf(fileOut,"%lf",arr[i][j]);
+                        fprintf(fileOut,"%lf",matrix[i][j]);
                 fprintf(fileOut, "\n");
             }
         }
 
         fclose(fileOut);
         return 1;
-    }
+
 }
 int getRowsNum(double ** matrix)
 {
-    return sizeof(matrix)/sizeof(matrix[0]);
+    int res = sizeof(matrix)/sizeof(matrix[0]); //не работает :(
+    return res;
 }
 int getColumnsNum(double ** matrix)
 {
@@ -182,7 +196,7 @@ double ** tranposeMatrix(double ** matrix)
     arr = (double **)malloc(getColumnsNum(matrix) * sizeof(double*));
 
     int i, j;
-    for (i = 0; i < m; i++)
+    for (i = 0; i < getColumnsNum(matrix); i++)
         arr[i]=(double *)malloc(getRowsNum(matrix) * sizeof(double));
 
     for (i = 0; i < getRowsNum(matrix); i++)
@@ -190,37 +204,126 @@ double ** tranposeMatrix(double ** matrix)
             arr[i][j] = matrix[j][i];
     return arr;
 }
-double ** combineMatrices(double ** matrix1, double ** matrix2, char * align)
+double ** combineMatrices(double ** matrix1, double ** matrix2, char * align) //todo ПНД
 {
     int i, j;
+    int at;
     double ** arr;
 
-    switch (align) {
-    case "top":
+    switch (align[0]) {
+    case 't':
         arr = (double **)malloc((getRowsNum(matrix1) + getRowsNum(matrix2)) * sizeof(double*));
-        for (i = 0; i < m; i++)
+        for (i = 0; i < getRowsNum(matrix1) + getRowsNum(matrix2); i++)
             arr[i]=(double *)malloc(getColumnsNum(matrix1) * sizeof(double));
 
         for (i = 0; i < getRowsNum(matrix2); i++)
             for (j = 0; j < getColumnsNum(matrix2); j++)
                 arr[i][j] = matrix2[i][j];
 
-        int at = getRowsNum(matrix2);
+        at = getRowsNum(matrix2);
         for (i = 0; i < getRowsNum(matrix1); i++)
             for (j = 0; j < getColumnsNum(matrix1); j++)
                 arr[i + at][j] = matrix2[i][j];
 
-        return arr;
         break;
-    case "left":
+    case 'l':
+        arr = (double **)malloc((getRowsNum(matrix1)) * sizeof(double*));
+        for (i = 0; i < getRowsNum(matrix1); i++)
+            arr[i]=(double *)malloc((getColumnsNum(matrix1) + getColumnsNum(matrix2)) * sizeof(double));
+
+        for (i = 0; i < getRowsNum(matrix2); i++)
+            for (j = 0; j < getColumnsNum(matrix2); j++)
+                arr[i][j] = matrix2[i][j];
+
+        at = getColumnsNum(matrix2);
+        for (i = 0; i < getRowsNum(matrix1); i++)
+            for (j = 0; j < getColumnsNum(matrix1); j++)
+                arr[i][j + at] = matrix1[i][j];
 
         break;
-    case "right":
+    case 'r':
+        arr = (double **)malloc((getRowsNum(matrix1)) * sizeof(double*));
+        for (i = 0; i < getRowsNum(matrix1); i++)
+            arr[i]=(double *)malloc((getColumnsNum(matrix1) + getColumnsNum(matrix2)) * sizeof(double));
+
+        for (i = 0; i < getRowsNum(matrix2); i++)
+            for (j = 0; j < getColumnsNum(matrix2); j++)
+                arr[i][j] = matrix1[i][j];
+
+        at = getColumnsNum(matrix2);
+        for (i = 0; i < getRowsNum(matrix1); i++)
+            for (j = 0; j < getColumnsNum(matrix1); j++)
+                arr[i][j + at] = matrix2[i][j];
 
         break;
-    case "bottom":
+    case 'b':
+        arr = (double **)malloc((getRowsNum(matrix1) + getRowsNum(matrix2)) * sizeof(double*));
+        for (i = 0; i < getRowsNum(matrix1) + getRowsNum(matrix2); i++)
+            arr[i]=(double *)malloc(getColumnsNum(matrix1) * sizeof(double));
+
+        for (i = 0; i < getRowsNum(matrix2); i++)
+            for (j = 0; j < getColumnsNum(matrix2); j++)
+                arr[i][j] = matrix2[i][j];
+
+        at = getRowsNum(matrix2);
+        for (i = 0; i < getRowsNum(matrix1); i++)
+            for (j = 0; j < getColumnsNum(matrix1); j++)
+                arr[i + at][j] = matrix2[i][j];
 
         break;
     default: printf("\ngo to default in combineMatrix\n"); return matrix1; break;
     }
+    //todo добавить free
+    return arr;
+}
+double ** multiplyByNumber(double ** matrix, double num)
+{
+    int i, j;
+    for (i = 0; i < getRowsNum(matrix); i++)
+        for (j = 0; j < getColumnsNum(matrix); j++)
+            matrix[i][j] *= num;
+    return matrix;
+}
+double ** multiplyByMatrix(double ** matrix1, double ** matrix2)
+{
+    double ** arr;
+    int i, j, m;
+
+    arr = (double **)malloc(getRowsNum(matrix1) * sizeof(double*));
+    for (i = 0; i < getRowsNum(matrix1); i++)
+        arr[i]=(double *)malloc(getColumnsNum(matrix2) * sizeof(double));
+
+
+    for(i = 0; i < getRowsNum(matrix1); i++)
+        for(j = 0; j < getColumnsNum(matrix1); j++)
+            for(m = 0; m < getRowsNum(matrix2); m++)
+                arr[i][j] = matrix1[i][j] * matrix2[i][m];
+    //todo free + ПНД
+    return arr;
+}
+double ** addMatrix(double ** matrix1, double ** matrix2)
+{
+    int i, j;
+
+    for(i = 0; i < getRowsNum(matrix1); i++)
+        for(j = 0; j < getColumnsNum(matrix2); j++)
+            matrix1[i][j] += matrix2[i][j];
+    return matrix1;
+}
+double ** powMatrix(double ** matrix, unsigned int degree)
+{
+    int i, j;
+    double ** arr;
+    arr = (double **)malloc(getRowsNum(matrix) * sizeof(double*));
+    for (i = 0; i < getRowsNum(matrix); i++)
+        arr[i]=(double *)malloc(getColumnsNum(matrix) * sizeof(double));
+
+    for(i = 0; i < getRowsNum(matrix); i++)
+        for(j = 0; j < getColumnsNum(matrix); j++)
+            arr[i][j] = matrix[i][j];
+
+    for (i = 0; i < degree; i++)
+        arr = addMatrix(arr, matrix);
+
+    return arr;
 }

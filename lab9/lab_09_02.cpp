b907c28e9f8abd94c4f41c71ba9fd185bbaf5bdd
Mike <mvngr@niuitmo.ru>
2016-12-14 12:54:09 +0300

    complete

--- lab9/lab_09_02.cpp
+++ lab9/lab_09_02.cpp
@@ -1,24 +1,27 @@
-#include <iostream>
-#include <string>
+#include <iostream> 
+#include <string> 
+#include <cstdlib> 
+#include <cmath> 
+#include <fstream> 
+#include <iomanip>
 #include <vector>
-#include <fstream>
 
 using namespace std;
 
 void getDim(string m, int & row, int & col) { //определяет размеры
     // в эту функцию передаются указатели на память
     // в следствии чего я изменяю col и row, не возвращая ничего
-    row = 0; col = 0;
+ row= 0; col = 0;
 
-    for (int i = 0; i < m.size(); i++){
+ for (int i= 0; i < m.size(); i++){
         switch (m[i]) {
-        case ';': row++; break;
-        case ',': col++; break;
-        default: break;
+ case ';': row++; break;
+ case ',': col++; break;
+ default: break;
         }
     }
     row++; //на одно значение меньше (в конце нет ";")
-    col /= row; //находим строки
+ col /= row; //находим строки
     col++;
     return;
 }
@@ -28,11 +31,11 @@ void printData(string s) {
     getDim(s, row, col);
     cout << '\n' << row << ',' << col << "\n\n";
 
-    int firstPoint = 0;
-    for(int i = 0; i < s.size(); i++) {
-        if(s[i] == ';') {
+ int firstPoint= 0;
+ for(int i= 0; i < s.size(); i++) {
+ if(s[i]== ';') {
             cout << s.substr(firstPoint, i - firstPoint) << '\n';
-            firstPoint = i+1;
+ firstPoint= i+1;
         }
     }
     cout << s.substr(firstPoint, s.size() - firstPoint) << '\n';
@@ -41,9 +44,9 @@ void printData(string s) {
 
 void editString(string & str, string from, string to) {
 
-    while (str.find(from) != -1) {
-    size_t t = str.find(from);
-    str = str.substr(0,t) + to + str.substr(t+from.size(), str.size());
+ while (str.find(from) != -1) {
+ size_t t= str.find(from);
+ str= str.substr(0,t) + to + str.substr(t+from.size(), str.size());
     }
     return;
 }
@@ -70,9 +73,14 @@ void andLogical(string & s) {
     return;
 }
 
+string specificToStringAB(int num) {
+ if (num==1) return "1";
+    else return "0";
+}
+
 void changeAB(string &s, int a, int b) {
-    editString(s,"a", to_string(a));
-    editString(s,"b", to_string(b));
+    editString(s,"a", specificToStringAB(a));
+    editString(s,"b", specificToStringAB(b));
     return;
 }
 
@@ -85,7 +93,7 @@ void getSimple(string &s) {
 
 void input(string s, int a, int b) {
     changeAB(s,a,b);
-    cout << s << " = ";
+ cout << s << "= ";
     getSimple(s);
     cout << s << endl;
     return;
@@ -93,28 +101,28 @@ void input(string s, int a, int b) {
 
 void fn(string s) {
     int a, b;
-    int & lFirst = a,
-        & lSecond = b; //ссылка на букву, по которой будет первый и второй цикл соответственно
+ int & lFirst= a,
+ & lSecond= b; //ссылка на букву, по которой будет первый и второй цикл соответственно
 
-    if (s.find('a')!=-1) {
-        a = 1;
-        lFirst = a;
-        lSecond = b;
+ if (s.find('a')!=-1) {
+ a= 1;
+ lFirst= a;
+ lSecond= b;
     }
     else {
-        a = -1;
-        lFirst = b;
-        lSecond = a;
+ a= -1;
+ lFirst= b;
+ lSecond= a;
     }
-    s.find('b')!=-1?b=1:b=-1;
+ s.find('b')!=-1?b=1:b=-1;
 
-    if(lFirst == lSecond == -1) //там нет букв, следовательно мы выводим поданный результат
+ if(lFirst== lSecond == -1) //там нет букв, следовательно мы выводим поданный результат
         return;
 
-    for (int i = 0; i <= lFirst; i++) {
-        if(lSecond==-1)
+ for (int i= 0; i <= lFirst; i++) {
+ if(lSecond==-1)
         input(s, i, -1);
-        for (int j = 0; j <= lSecond; j++)
+ for (int j= 0; j <= lSecond; j++)
             input(s,i,j);
     }
 
@@ -123,33 +131,33 @@ void fn(string s) {
 void headerPrint(string s) {
 
     int st;
-    cout << "header code:\n";
-    while(s.find(") {") != -1) {
-        size_t t = s.find(") {");
-        for(int i = t; i != 0; i--)
-            if (s[i] == '\n') {
-                st = i;
+ cout << "header code:\n";
+ while(s.find(") {") != -1) {
+ size_t t= s.find(") {");
+ for(int i= t; i != 0; i--)
+ if (s[i]== '\n') {
+ st= i;
                 break;
             }
         cout << s.substr(st, t - st + 1);
-        s[t] = '.';
+ s[t]= '.';
     }
 }
 
 void coursePrint(string s) {
 
     int finish;
-    cout << "\n\ncourse code:\n\n";
+ cout << "\n\ncourse code:\n\n";
 
-    while(s.find(") {") != -1) {
-        size_t t = s.find(") {");
-        for(int i = t; i < s.size() - 1; i++)
-            if (s[i] == '}' && s[i+1] == '\n') {
-                finish = i;
+ while(s.find(") {") != -1) {
+ size_t t= s.find(") {");
+ for(int i= t; i < s.size() - 1; i++)
+ if (s[i]== '}' && s[i+1] == '\n') {
+ finish= i;
                 break;
             }
         cout << s.substr(t+4, finish - t - 5) << "\n\n";
-        s[t] = '.';
+ s[t]= '.';
     }
 }
 
@@ -162,61 +170,80 @@ void splitHC(string s) {
 }
 
 void divide(int n) {
-    int res = 0;
-    for(int i = 1; i <= 1000; i++) {
-        i%n==0?res++:false; //false для того, чтобы он ничего не делал. Можно написать true, ничего не изменится
+ int res= 0;
+ for(int i= 1; i <= 1000; i++) {
+ i%n==0?res++:false; //false для того, чтобы он ничего не делал. Можно написать true, ничего не изменится
     }
     cout << '\n' << res << endl;
 }
 
-vector<vector<int>> badMatrix() {
-    vector<vector<int>> a;
+vector<vector<int> > badMatrix() {
+    vector<vector<int> > a;
     return a;
 }
 
-vector<vector<double>> readMatrix (string link) {
-	ifstream fileIn(link, ios::in);
+vector<vector<double> > readMatrix (char * link) {
+ ifstream fileIn(link, ios::in);
 	int n, m;
-	charr c;
+	char c;
 	fileIn >> m >> c >> n;
 	vector<vector<double> > arr (m);
-	for (int i = 0; i < m; i++) {
+ for (int i= 0; i < m; i++) {
 		arr[i].resize(n);
-		for (int j = 0; j < n; j++) {
+ for (int j= 0; j < n; j++) {
 			fileIn >> arr[i][j];
 		}
 	}
 	return arr;
 }
 
-vector<vector<double>> multiplyByMatrix(vector<vector<double>> matrix1, vector<vector<double>> matrix2) {
-	int m1 = matrix1.size(),
-		n1 = matrix1[0].size(),
-		m2 = matrix2.size(),
-		n2 = matrix2[0].size();
-	if (n1 != m2) {
-		printf("\n-->Multiply_by_Matrix_ERROR\n\n");
+double sum (double a, double b) {
+	return a + b;
+}
+
+void fn_or() {
+ for (bool x= 0; x < 1; x++) {
+ for (bool y= 0; y < 1; y++) {
+			cout << (x || y) << "\n";
+		}
 	}
-	vector<vector<double>> arr;
+}
+
+void fn_and() {
+ for (bool x= 0; x < 1; x++) {
+ for (bool y= 0; y < 1; y++) {
+			cout << (x && y) << "\n";
+		}
+	}
+}
+
+vector<vector<double> > multiplyByMatrix(vector<vector<double> > matrix1, vector<vector<double> > matrix2) {
+ int m1= matrix1.size(),
+ n1= matrix1[0].size(),
+ m2= matrix2.size(),
+ n2= matrix2[0].size();
+ if (n1 != m2) 
+		printf("\nmultiply err\n\n");
+	vector<vector<double> > arr;
 	arr.resize(m1);
-	for (int i = 0; i < m1; i++) {
+ for (int i= 0; i < m1; i++) {
 		arr[i].resize(n2);
-		for (int j = 0; j < n2; j++) {
-			double sum = 0.0;
-			for (int k = 0; k < n1; k++) {
-				sum += matrix1[i][k] * matrix2[k][j];
+ for (int j= 0; j < n2; j++) {
+ double sum= 0.0;
+ for (int k= 0; k < n1; k++) {
+ sum += matrix1[i][k] * matrix2[k][j];
 			}
-			arr[i][j] = sum;
+ arr[i][j]= sum;
 		}
 	}
 	return arr;
 }
 
-void saveMatrix(vector<vector<double>> arr, string link) {
-    ofstream fileOut(link, ios::out);
+void saveMatrix(vector<vector<double> > arr, char * link) {
+ ofstream fileOut(link, ios::out);
 	fileOut << arr.size() << ',' << arr[0].size() << "\n";
-	for (int i = 0; i < arr.size(); i++) {
-		for (int j = 0; j < arr[0].size(); j++) {
+ for (int i= 0; i < arr.size(); i++) {
+ for (int j= 0; j < arr[0].size(); j++) {
 			fileOut << arr[i][j];
 			if (j < arr[0].size() - 1) {
 				fileOut << ',';
@@ -233,19 +260,19 @@ int main() {
     setlocale(LC_ALL, "rus");
 
     //#3
-    //cin >> m;
-    //printData(m);
-    //m.clear();
+    cin >> m;
+    printData(m);
+    m.clear();
     //#4
-    //cin >> m;
-    //fn(m);
-    //m.clear();
+    cin >> m;
+    fn(m);
+    m.clear();
     //#5
-    //m = "#include <iostream>\ndouble sum (double a, double b) {\nreturn a + b;\n}\nint main () {\ncout << sum (3, 2) << endl;\nreturn 0;\n}\n";
-    //splitHC(m);
+    m= "#include <iostream>\ndouble sum (double a, double b) {\nreturn a + b;\n}\nint main () {\ncout << sum (3, 2) << endl;\nreturn 0;\n}\n";
+    splitHC(m);
     //#6
-    //cin >> n;
-    //divide(n);
+    cin >> n;
+    divide(n);
     //#7
     saveMatrix(multiplyByMatrix(readMatrix("matrix1.txt"), readMatrix("matrix2.txt")), "matrix3.txt");
 

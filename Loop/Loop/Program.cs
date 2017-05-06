86d863bd8096e29d5746f87666c618b850635d22
Mike <mvngr@niuitmo.ru>
2017-05-06 17:24:16 +0300

    + update lab

--- Loop/Loop/Program.cs
+++ Loop/Loop/Program.cs
@@ -167,6 +167,36 @@ namespace Loop
 
             Console.WriteLine("Сумма чисел равна {0}", s);
             */
+
+            /*
+            
+            Новая лаба
+            ==========
+
+            */
+            /*
+            int[] myArray = { 100, 1, 32, 3, 14, 25, 6, 17, 8, 99 };
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] % 2 == 0)
+                    myArray[i] = 0;
+                Console.Write("{0} ", myArray[i]);
+            } 
+            */
+            int[] MyArray;
+            int n = int.Parse(Console.ReadLine());
+            MyArray = new int[n];
+            for (int i = 0; i < MyArray.Length; ++i)
+            {
+                Console.Write("a[{0}]=", i);
+                MyArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int x in MyArray)
+                Console.Write("{0} ", x);
+
+
             Console.Write("\n*****\nСтрельбище\n\n");
 
             List<Shot> shots_log = new List<Shot>();

2dd8887bba15e568256876d5ffedfe72faa8862e
Mike <mvngr@niuitmo.ru>
2017-05-13 16:50:35 +0300

    +2 labs

---
+++ Igra/Igra/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            Gamer g1 = new Gamer("Niko");
+            for (int i = 1; i <= 6; i++)
+                Console.WriteLine("Выпало количество очков {0} для игрока {1}", g1.SeansGame(), g1.ToString());
+    */
+            ArithmeticProgression ap = new ArithmeticProgression();
+
+            Console.WriteLine("{0}", ap.GetElement(3));
+            Console.WriteLine("{0}", ap.GetElement(3, 2, 2));
+
+            GeometricProgression gp = new GeometricProgression();
+
+            Console.WriteLine("{0}", gp.GetElement(8));
+            Console.WriteLine("{0}", gp.GetElement(1, 10, 2));
+
+            Console.ReadKey();
+        }
+
+        class IgralnayaKost
+        {
+            Random r;
+
+            public IgralnayaKost()
+            {
+                r = new Random();
+            }
+            public int random()
+            {
+                int res = r.Next(6) + 1;
+                return res;
+            }
+
+        }
+        class Gamer
+        {
+            string Name;
+            IgralnayaKost seans;
+
+            public Gamer(string name)
+            {
+                Name = name;
+                seans = new IgralnayaKost();
+            }
+            public int SeansGame()
+            {
+                return seans.random();
+            }
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+        abstract class Progression
+        {
+            public abstract int GetElement(int k);
+        }
+
+        class ArithmeticProgression : Progression
+        {
+            public ArithmeticProgression() { }
+            private int calc(int start, int k, int d)
+            {
+                return start + (k - 1) * d;
+            }
+            public override int GetElement(int k)
+            {
+                return calc(0, k, 1);
+            }
+            public int GetElement(int start, int k, int d)
+            {
+                return calc(start, k, d);
+            }
+        }
+        class GeometricProgression : Progression
+        {
+            public GeometricProgression() { }
+
+            private int calc(int start, int k, int d)
+            {
+                return start * (int) Math.Pow((double)d, (double)(k - 1));
+            }
+            public override int GetElement(int k)
+            {
+                return calc(1, k, 2);
+            }
+            public int GetElement(int start, int k, int d)
+            {
+                return calc(start, k, d);
+            }
+        }
+    }
+}
---
+++ MyClassLine/MyClassLine/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.Show();
+            Point p2 = new Point(12, 13);
+            p2.Show();
+
+            Line line = new Line(p1, p2);
+            line.Show();
+
+            double dtr = line.DlinL();
+            Console.WriteLine("Длина отрезка " + dtr);
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Point
+    {
+        private double x;
+        private double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x; this.y = y;
+        }
+        public Point()
+        { }
+        public void Show()
+        {
+            Console.WriteLine("Точка с координатами: ({0}, {1})", x, y);
+        }
+        public double Dlina(Point p1, Point p2)
+        {
+            double Dl = Math.Sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+            return Dl;
+        }
+        public override string ToString()
+        {
+            string ss = x + " ; " + y;
+            return ss;
+        }
+    }
+
+    class Line
+    {
+        private Point pStart = new Point();
+        private Point pEnd = new Point();
+
+        public Line(Point pStart, Point pEnd)
+        {
+            this.pStart = pStart;
+            this.pEnd = pEnd;
+        }
+        public Line()
+        { }
+        public void Show()
+        {
+            Console.WriteLine("Отрезок с координатами: ({0}) - ({1})", pStart, pEnd);
+        }
+        public double DlinL()
+        {
+            return pStart.Dlina(pEnd, pStart);
+        }
+
+    }
+}

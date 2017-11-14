f9da7b71b4d50eb54672194f6317f3a468f2f380
Mike <mihailqqqq@gmail.com>
2017-11-15 02:03:58 +0300

    +lab

---
+++ lab6-2017/lab6-2017/Algorithm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab6_2017
+{
+	public class Algorithm
+	{
+		public Algorithm()
+		{
+			a = new List<List<int>>();
+			minH = new List<int>();
+		}
+
+		public List<List<int>> a;
+		
+		private int minSum;
+		public int MinSum
+		{
+			get { return minSum; }
+			set {}
+		}
+
+		private List<int> minH; 
+		public List<int> MinH
+		{
+			get { return minH; }
+			set {}
+		}
+
+		private int FullTrip(List<int> h, int sum)
+		{
+			for(int i = 0; i < a.Count; i++)
+				if (h.IndexOf(i) == -1)
+					if (a[h.Last()][i] != Int32.MaxValue)
+					{
+						List<int> copyH = new List<int>();
+						copyH.AddRange(h);
+						copyH.Add(i);
+						if (copyH.Count == a.Count && a[copyH.Last()][copyH.First()] != Int32.MaxValue)
+						{
+							sum += a[h.Last()][copyH.Last()];
+							sum += a[copyH.Last()][copyH.First()];
+							if (sum < minSum)
+							{
+								minSum = sum;
+								minH.Clear();
+								minH.AddRange(copyH);
+							}
+							return -1;
+						}
+						else
+						{
+							FullTrip(copyH, (sum + a[h.Last()][i]));
+							continue;
+						}
+							
+					}
+			return -1;
+		}
+
+		public void StartFullTrip()
+		{
+			minSum = Int32.MaxValue;
+			FullTrip(new List<int>() {0}, 0);
+		}
+	}
+}
---
+++ lab6-2017/lab6-2017/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab6_2017
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random r = new Random();
+            List<List<int>> arr = new List<List<int>>();
+            arr.Add(new List<int>(){0,11,49,32});
+            arr.Add(new List<int>(){49,0,47,23});
+            arr.Add(new List<int>(){94,87,0,99});
+            arr.Add(new List<int>(){74,67,32,0});
+            /*for (int i = 0; i < 4; i++)
+            {
+                arr.Add(new List<int>());
+                for (int j = 3; j >= 0; j--)
+                {
+                    if(i != 3 - j)
+                        arr[i].Add(j+2);
+                    else
+                        arr[i].Add(0);
+                }
+            }*/
+            Algorithm alg = new Algorithm();
+            alg.a.AddRange(arr);
+            alg.StartFullTrip();
+            Console.WriteLine("MS: " + alg.MinSum);
+            alg.MinH.ForEach(delegate(int i) { Console.Write(i + " -> "); });
+            
+
+        }
+    }
+}

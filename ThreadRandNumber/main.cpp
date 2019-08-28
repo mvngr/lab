908fcf619dd961444965fa6c3059a31d1d73339a
Mike <mvngr@niuitmo.ru>
2019-08-28 11:54:24 +0300

    Переделал переменную на атомарный объект

--- ThreadRandNumber/main.cpp
+++ ThreadRandNumber/main.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <mutex>
 #include <chrono>
+#include <atomic>
 
 static std::mutex m; //мьютекс для записи результата
 static std::mutex out; //мьютекс для вывода в паралельных действиях
@@ -17,20 +18,20 @@ unsigned int generateValue()
     return static_cast<unsigned int>( ( rand() % maxNumber + 1 ) );
 }
 
-void doGenerate(const unsigned int &inId, const unsigned int &inInputValue, const unsigned int &inIterations, const unsigned int &inDelay, unsigned int &inResult)
+void doGenerate(const unsigned int &inId, const unsigned int &inInputValue, const unsigned int &inIterations, const unsigned int &inDelay, std::atomic<unsigned int> &inResult)
 {
     for(unsigned int i = 0; i < inIterations; i++)
     {
-        if(inResult != maxNumber + 1) //когда число найдено - прекращаем цикл
+        if(inResult.load() != maxNumber + 1) //когда число найдено - прекращаем цикл
             break;
 
         unsigned int v = generateValue(); //генерируем число
         if(v == inInputValue) //если числа совпадают
         {
             m.lock(); //лочим для записи
-            if(inResult == maxNumber+1) //еще раз проверяем, ибо пока шли до lock'а могли уже поступить записи
+            if(inResult.load() == maxNumber+1) //еще раз проверяем, ибо пока шли до lock'а могли уже поступить записи
             {
-                inResult = i; //пишем
+                inResult.store(i); //пишем
             }
             m.unlock(); //отпускаем мьютекс
         }
@@ -50,8 +51,6 @@ int main()
     unsigned int iteration;
     unsigned int delay;
 
-
-
     std::cout << "Введите число в диапазоне от 1 до " << maxNumber << ": ";
     std::cin >> inputValue;
     std::cout << "Введите общее количество итераций для всех потоков: ";
@@ -59,7 +58,7 @@ int main()
     std::cout << "Введите квант времени [ms]: ";
     std::cin >> delay;
 
-    unsigned int result = maxNumber + 1; //делаем изначальный результат недостижимым. Можно было сделать при помощи bool делать проверку, но тогда на один параметр больше бы стало
+    std::atomic<unsigned int> result(maxNumber + 1); //делаем изначальный результат недостижимым. Можно было сделать при помощи bool делать проверку, но тогда на один параметр больше бы стало
     unsigned int threadCount = std::thread::hardware_concurrency(); //можно и запрашивать количество, но удобнее использовать количество тредов, доступных железу
 
     std::vector<std::shared_ptr<std::thread>> listThreads; //хранитель тредов
@@ -75,8 +74,8 @@ int main()
     auto end = std::chrono::high_resolution_clock::now(); //замечаем конец
     std::cout << "Время выполнения:" << std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count() << " ms" << std::endl;
 
-    if(result < maxNumber) //проверка на корректное нахождение результата
-        std::cout << "Победитель: " << result << std::endl;
+    if(result.load() < maxNumber) //проверка на корректное нахождение результата
+        std::cout << "Победитель: " << result.load() << std::endl;
     else
         std::cout << "Никто не выйграл" << std::endl;
 
